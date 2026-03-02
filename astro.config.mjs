f26225b4c9e37572ec2fc75d3f17e0ab73077585
Yanko Shterev <yshterev@gmail.com>
2026-03-02 21:13:17 +0200

    Add Tailwing + Daisy UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig, fontProviders } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   fonts: [{
     provider: fontProviders.google(),
     name: "Roboto",
     cssVariable: "--font-roboto"
   }],
+
   devToolbar: {
     enabled: false,
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [daisyui],
+};
+

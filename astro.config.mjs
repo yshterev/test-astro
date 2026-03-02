6baaaaa535a216563715db6b35a2da7e8501b84b
Yanko Shterev <yshterev@gmail.com>
2026-03-02 21:38:42 +0200

    Try font/css config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,10 @@ export default defineConfig({
   fonts: [{
     provider: fontProviders.google(),
     name: "Roboto",
-    cssVariable: "--font-roboto"
+    cssVariable: "--font-roboto",
+    weights: [400, 500],
+    styles: ["normal"],
+    subsets: ["latin", "cyrillic"],
   }],
 
   devToolbar: {

181c9476a3643cafd4662833194808a502c33ed7
Yanko Shterev <yshterev@gmail.com>
2026-03-02 23:39:34 +0200

    Add prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,16 @@ import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  fonts: [{
-    provider: fontProviders.google(),
-    name: "Roboto",
-    cssVariable: "--font-roboto",
-    weights: [400, 500],
-    styles: ["normal"],
-    subsets: ["latin", "cyrillic"],
-  }],
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Roboto",
+      cssVariable: "--font-roboto",
+      weights: [400, 500],
+      styles: ["normal"],
+      subsets: ["latin", "cyrillic"],
+    },
+  ],
 
   devToolbar: {
     enabled: false,
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,14 +2,13 @@ import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [daisyui],
-	daisyui: {
-		themes: ["dark"],
-		darkTheme: "dark",
-	},
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["dark"],
+    darkTheme: "dark",
+  },
 };
-
